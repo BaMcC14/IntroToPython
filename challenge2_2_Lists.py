8b90f5e292de0e765d308607da853808bae1fa20
Blake McCann <blake.mccann@pgip-tech.com>
2023-04-14 09:06:10 -0500

    Adding Section Four Lists

---
+++ challenge2_2_Lists.py
@@ -0,0 +1,98 @@
+#list 
+#stringList = ['Louisiana', 'Texas', 'Mississippi']
+#numberList = [1, 4, 5, 2, 7, 3, 2]
+#mixedList = [1,3, 'Louisiana', 3.53, [10, 'anotherList']]
+#emptyList = []
+
+#petsList = ['dog', 'cat', 'fish']
+#print(petsList[2])
+
+#numberList2 = [17, 123]
+#numberList2[1] = 5
+#print(numberList2)
+
+#petList = ['dog', 'cat', 'fish']
+#petList[1] = 'hamster'
+#print(petList)
+
+#firstList = [1, 2, 3]
+#secondList = [4, 5, 6]
+#thirdList = firstList + secondList
+#print(thirdList)
+
+#myList = [0]
+#resultList = myList * 4
+#print(resultList)
+
+#petsList = ['dog', 'cat', 'fish','rabbit', 'hamster', 'bird']
+#petsList.remove('rabbit')
+#print(petsList)
+
+#numList = [2, 45, 9, 17, 1, 4]
+#print("Max: ",max(numList))
+#print("Min: ",min(numList))
+#print("Length: ", len(numList))
+#print("Sum: ", sum(numList))
+#print("Average: ", sum(numList)/len(numList))
+
+#myPets = ['hampster', 'cat', 'dog', 'horse', 'cow', 'chickens']
+
+#numList = [2, 45, 9, 17, 1, 4]
+#listElement = iter(numList)
+#print(next(listElement))
+#print(next(listElement))
+#print(next(listElement))
+#print(next(listElement))
+#print(next(listElement))
+#print(next(listElement))
+
+#nums = [1, 2, 3]
+#for num in num:
+#  print(num)
+#print(dir(nums))
+
+#mySet = {'fun', 'sun', 'run'}
+#mySet.add('bun')
+#print(mySet)
+
+#myPets = {'dog', 'cat', 'rabbit'}
+#myPets.discard('dog')
+#print(myPets)
+#myPets.discard('dinosaur')
+#print(myPets)
+
+#myTuple = ('ice cream', 'frozen yogurt', 'sorbet')
+#print(myTuple)
+
+#lonelyTuple = ('ice cream',)
+#print(lonelyTuple)
+#print(type(lonelyTuple))
+
+#eng2span = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+#myVals = list(eng2span.values())
+#print('uno' in myVals)
+
+#multiplesList = [[1,2,3,4,5],
+ #                [2,4,6,8,10], 
+  #               [3,6,9,12,15], 
+   #              [4,8,12,16,20]]
+#print(multiplesList[3][2])
+#multiplesList.append([5,10,15,20,25])
+#print(multiplesList)
+#multiplesList[0].append(6)
+#print(multiplesList)
+
+#myString = '    This string contains to many spaces       ' 
+#print(myString.strip())
+
+#myList = ['ice cream', 'frozen yogurt', 'sorbet']
+#myList.sort()
+#print(myList)
+
+myFirstList = ['ice cream', 'frozen yogurt', 'sorbet']
+anotherList = myFirstList[:]
+anotherList.sort()
+myElement = 'gelato'
+print(anotherList)
+print(myFirstList)
+
