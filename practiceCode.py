ba6226a9c6763d57de8341d57add679817c87deb
Blake McCann <blake.mccann@pgip-tech.com>
2023-04-17 12:42:57 -0500

    Adding Over view of Section 2

---
+++ practiceCode.py
@@ -0,0 +1,45 @@
+# temp1 = 25
+# temp2 = 40
+
+# #or Logical Operator 
+# #only one needs to be true
+# if temp1 <= 0 or temp1 >= 30:
+#     print("The temperature is bad")
+# else:
+#     print("The temperature is good")
+
+# #and Logical Operator
+# #both need to be true
+# if temp2 > 0 and temp2 < 30:
+#     print("The temperature is good")
+# else:
+#     print("The temperature is bad")
+
+# sunny = True
+
+# #not Logical Operator
+# if sunny:
+#     print("It is sunny")
+# else:
+#     print("It is cloudy")
+
+# test = ''
+# print(bool(test))
+# bool(test)
+
+# myBadList = [1]
+# for addElement in range(1, 11):
+#     myBadList.append(addElement + 1)
+#     print("Made it in")
+# print("Made it out")
+
+# test = ''
+# for check in test:
+#     print('Made it in')
+# print('Made it out')
+
+# for nums in range(3,1,-1):
+#     print(nums)
+
+
+
